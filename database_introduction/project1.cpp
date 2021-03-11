0a50481e0173e391a94be39b88836d123983f631
hsshhsxm <hsshhsxm@gmail.com>
2021-03-11 15:48:06 +0800

    define Student and Score struct

---
+++ database_introduction/project1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+    char ID[10];
+    char name[18];
+    char sex[2];
+    char birthday[8];
+    char collage[20];
+    char department[20];
+
+};
+struct Score
+{
+    char ID[10];
+    char coursename[20];
+    int score;
+};
+
+
+int main()
+{
+    
+    return 0;
+}
