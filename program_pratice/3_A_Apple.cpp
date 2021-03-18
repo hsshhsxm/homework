11db35170679d2be5e2ec5dd7019fe495a37211c
Sullivan Suen <hsshhsxm@users.noreply.github.com>
2021-03-18 17:17:07 +0800

    init 3A

---
+++ program_pratice/3_A_Apple.cpp
@@ -0,0 +1,31 @@
+/*
+Please complete the following program to ensure that the output is 4 5 1.
+*/
+#include <iostream>
+using namespace std;
+class Apple {
+    int nTotalNumber;
+    //Apple(){}
+    //Apple(Apple & a){}
+
+
+static void PrintTotal() {
+		cout << nTotalNumber << endl; 
+	}
+
+};
+int Apple::nTotalNumber = 0;
+Apple Fun(const Apple & a) {
+	a.PrintTotal();
+	return a;
+}
+int main()
+{
+	Apple * p = new Apple[4];
+	Fun(p[2]);   // 4
+	Apple p1,p2;
+	Apple::PrintTotal ();   // 5
+	delete [] p;
+	p1.PrintTotal ();  // 1
+	return 0;
+}
