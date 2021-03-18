e551b79a76768a1d6f9e313e1ccddd992d7e021a
Sullivan Suen <hsshhsxm@users.noreply.github.com>
2021-03-18 17:31:57 +0800

    init 3B

---
+++ program_pratice/3_B_Program_Completion.cpp
@@ -0,0 +1,29 @@
+/*
+输入
+The input is composed of multiple cases, and each case occupies a single line, which contains two integers m and n.
+输出
+The first line is 123.
+Then for each case, the output contains two lines with m in first line and n in second line.
+*/
+#include <iostream>
+using namespace std;
+class A {
+public:
+	int val;
+
+	A(int
+// 在此处补充你的代码
+};
+int main()
+{
+	int m,n;
+	A a;
+	cout << a.val << endl;
+	while(cin >> m >> n) {
+		a.GetObj() = m;
+		cout << a.val << endl;
+		a.GetObj() = A(n);
+		cout << a.val<< endl;
+	}
+	return 0;
+}
