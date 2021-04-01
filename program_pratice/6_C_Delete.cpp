0aa4230cd5177036da6c706f16f468f16a715d22
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-04-01 15:14:41 +0800

    init 6A 6B 6C 6D

---
+++ program_pratice/6_C_Delete.cpp
@@ -0,0 +1,25 @@
+/*
+destructor B
+destructor A
+*/
+
+
+#include <iostream> 
+using namespace std;
+class A 
+{ 
+public:
+	A() { }
+// 在此处补充你的代码
+}; 
+class B:public A { 
+	public: 
+	~B() { cout << "destructor B" << endl; } 
+}; 
+int main() 
+{ 
+	A * pa; 
+	pa = new B; 
+	delete pa; 
+	return 0;
+}
