6a1211c29abe03458308f0f761c5f68aeef07faa
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-04-23 01:42:04 +0800

    finish

--- program_pratice/bmp旋转作业/rotatebmp.cpp
+++ program_pratice/bmp旋转作业/rotatebmp.cpp
@@ -12,7 +12,7 @@ typedef long LONG;
 //bitmap header
 typedef struct tagBITMAPFILEHEADER
 {
-	WORD bfType; // 位图文件的类型，必须为“BM”
+	//WORD bfType; // 位图文件的类型，必须为“BM”
 	DWORD bfSize; // 位图文件的大小，以字节为单位
 	WORD bfReserved1; // 位图文件保留字，必须为 0
 	WORD bfReserved2; // 位图文件保留字，必须为 0
@@ -22,14 +22,14 @@ typedef struct tagBITMAPFILEHEADER
 //bbitmap info header
 typedef struct tagBITMAPINFOHEADER{
 	DWORD biSize; // 本结构所占用字节数
-	LONG biWidth; // 位图的宽度，以像素为单位
-	LONG biHeight; // 位图的高度，以像素为单位
+	DWORD biWidth; // 位图的宽度，以像素为单位
+	DWORD biHeight; // 位图的高度，以像素为单位
 	WORD biPlanes; // 目标设备的平面数不清，必须为 1
 	WORD biBitCount;// 每个像素所需的位数，必须是 1(双色), 4(16 色)，8(256 色)或 24(真彩色)之一 
 	DWORD biCompression; // 位图压缩类型，必须是 0(不压缩),1(BI_RLE8 压缩类型)或 2(BI_RLE4 压缩类型)之一
 	DWORD biSizeImage; // 位图的大小，以字节为单位
-	LONG biXPelsPerMeter; // 位图水平分辨率，每米像素数 
-	LONG biYPelsPerMeter; // 位图垂直分辨率，每米像素数 
+	DWORD biXPelsPerMeter; // 位图水平分辨率，每米像素数 
+	DWORD biYPelsPerMeter; // 位图垂直分辨率，每米像素数 
 	DWORD biClrUsed;// 位图实际使用的颜色表中的颜色数
 	DWORD biClrImportant;// 位图显示过程中重要的颜色数 
 }BITMAPINFOHEADER;
@@ -45,8 +45,7 @@ class Bmp{
 public:
 	BITMAPFILEHEADER head;
 	BITMAPINFOHEADER info;
-	int height,width,pixelSize;
-	int widthModified;
+	int height,width,size;
 	WORD type;
 	DATA * data;
 	Bmp(){}
@@ -54,64 +53,101 @@ public:
         if(data)
             delete [] data;
     }
-	void readFile(char* fileName);
-	void rotate(char * targetFile);
+	int readFile(const char* fileName);
+	void rotate(const char * targetFile = "dest.bmp");
+	int getToAdd(BITMAPINFOHEADER & info);
 };
 
 
 int main(int argc,char * argv[]){
     Bmp bf;
-    bf.readFile(argv[1]);
-    bf.rotate(argv[2]);
+	if(argv[1]){
+		int judge = bf.readFile(argv[1]);
+		if(judge == 0){		
+			if(argv[2])
+				bf.rotate(argv[2]);
+			else 
+				bf.rotate();
+		}
+		else{
+			cout << "No such file" << endl;
+		}
+	}
+	else{
+		cout << "No such file" << endl;
+	}
+    
     return 0;
 }
 
-void Bmp::readFile(char* fileName){
+int Bmp::getToAdd(BITMAPINFOHEADER & info){
+	int line = (info.biWidth * info.biBitCount + 31) / 8;
+	line -= line % 4;
+	int toAdd = line - (info.biWidth * info.biBitCount) / 8;
+	return toAdd;
+}
+
+int Bmp::readFile(const char* fileName){
 	FILE * fp;
 	fp = fopen(fileName, "rb");
+	if(!fp){
+		return -1;
+	}
+	//cout << "open file: " << fileName << endl;
 	//read file type
 	fread(&type,1,sizeof(WORD),fp);
+	//cout << "type=" << type << endl;
 	//read header and info
 	fread(&head,1,sizeof(BITMAPFILEHEADER),fp);
 	fread(&info,1,sizeof(BITMAPINFOHEADER),fp);
+	//cout << "read file: " << fileName << endl;
 	height = info.biHeight;
 	width = info.biWidth;
-	//每一行存储像素信息的字节数必须是4的倍数，不够就填0补上
-	if(width%4 == 0)
-		widthModified = width;
-	else  
-		widthModified = (width / 4 + 1) * 4;
-	//3 bytes per pixel
-	pixelSize = info.biSizeImage / 3;
-	data = new DATA[pixelSize];
+	size = height * width;
+	//cout << "old h,w=" << height << "," << width << endl;
+	//每一行存储像素信息的字节数必须是4的倍数
+	int toAdd = getToAdd(info);
+	data = new DATA[size];
 	//read pixel data
-	fread(data,1,sizeof(DATA)*pixelSize,fp);
+	for(int i = 0; i < height; ++i){
+		fread((char*)data + i * width * 3, 3, width, fp);
+		fseek(fp, toAdd, SEEK_CUR);
+	}
+	//cout << "readed file: "<< data[2].blue << endl;
 	fclose(fp);
+	return 0;
 }
 
-void Bmp::rotate(char * targetFile){
-	int newHeight = widthModified;
+void Bmp::rotate(const char * targetFile){
+	//cout << "rotate begin" << endl;
+	int newHeight = width;
 	int newWidth = height;
     BITMAPFILEHEADER newHead = head;
     BITMAPINFOHEADER newInfo = info;
 	newHead.bfSize = (DWORD) (newHead.bfSize);
     newInfo.biHeight = (DWORD) newHeight;
     newInfo.biWidth = (DWORD) newWidth;
-    newInfo.biSizeImage = (DWORD) (pixelSize * 3);
+	int toAdd = getToAdd(newInfo);
+    newInfo.biSizeImage = (DWORD) ((newInfo.biWidth*3+toAdd) * (newInfo.biHeight));
+	//cout << "设置参数完成" << endl;
+	DATA *target = new DATA[newHeight * newWidth];
+	//cout << "new h,w=" << newHeight << "," << newWidth << endl;
+	for (int i=0; i<newHeight; ++i){
+        for (int j=0; j<newWidth; ++j){
+			//cout << "i,j=" << i<< "," <<j<< endl;
+            *(target + i * newWidth + j) = *(data + j * width + newHeight - i - 1);
+        }
+    }
+	//cout << "旋转完成" << endl;
 	FILE * fp = fopen(targetFile,"wb");
-	
-	DATA *target = new DATA[pixelSize];
-	memset(target,0,sizeof(DATA)*pixelSize);
-	for(int i = 0; i < newHeight; ++i){
-		for(int j = 0; j < newWidth; ++j){
-			target[i * newWidth + j] = data[j*widthModified + newHeight - i - 1];
-		}
-	}
-
-    fwrite(&type,1,sizeof(WORD),fp);
+	fwrite(&type,1,sizeof(WORD),fp);
     fwrite(&newHead,1,sizeof(BITMAPFILEHEADER),fp);
     fwrite(&newInfo,1,sizeof(BITMAPINFOHEADER),fp);
-    fwrite(target,1,sizeof(DATA) * pixelSize,fp);
+	char s = '0';
+	for(int i = 0; i < newHeight; ++i){
+		fwrite((char*)target + i * newWidth * 3, 3, newWidth, fp);
+		fwrite(&s, 1, toAdd, fp);
+	}
     fclose(fp);
     delete[] target;
 }
