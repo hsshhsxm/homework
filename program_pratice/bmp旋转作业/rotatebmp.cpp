ecf5ac0ad3e4b7c131f19e8206e29ccb3a852db4
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-04-23 14:22:22 +0800

    compile

--- program_pratice/bmp旋转作业/rotatebmp.cpp
+++ program_pratice/bmp旋转作业/rotatebmp.cpp
@@ -25,13 +25,13 @@ typedef struct tagBITMAPINFOHEADER{
 	DWORD biWidth; // 位图的宽度，以像素为单位
 	DWORD biHeight; // 位图的高度，以像素为单位
 	WORD biPlanes; // 目标设备的平面数不清，必须为 1
-	WORD biBitCount;// 每个像素所需的位数，必须是 1(双色), 4(16 色)，8(256 色)或 24(真彩色)之一 
+	WORD biBitCount;// 每个像素所需的位数，必须是 1(双色), 4(16 色)，8(256 色)或 24(真彩色)之一
 	DWORD biCompression; // 位图压缩类型，必须是 0(不压缩),1(BI_RLE8 压缩类型)或 2(BI_RLE4 压缩类型)之一
 	DWORD biSizeImage; // 位图的大小，以字节为单位
-	DWORD biXPelsPerMeter; // 位图水平分辨率，每米像素数 
-	DWORD biYPelsPerMeter; // 位图垂直分辨率，每米像素数 
+	DWORD biXPelsPerMeter; // 位图水平分辨率，每米像素数
+	DWORD biYPelsPerMeter; // 位图垂直分辨率，每米像素数
 	DWORD biClrUsed;// 位图实际使用的颜色表中的颜色数
-	DWORD biClrImportant;// 位图显示过程中重要的颜色数 
+	DWORD biClrImportant;// 位图显示过程中重要的颜色数
 }BITMAPINFOHEADER;
 
 //pixel data
@@ -63,10 +63,10 @@ int main(int argc,char * argv[]){
     Bmp bf;
 	if(argv[1]){
 		int judge = bf.readFile(argv[1]);
-		if(judge == 0){		
+		if(judge == 0){
 			if(argv[2])
 				bf.rotate(argv[2]);
-			else 
+			else
 				bf.rotate();
 		}
 		else{
@@ -76,7 +76,7 @@ int main(int argc,char * argv[]){
 	else{
 		cout << "No such file" << endl;
 	}
-    
+
     return 0;
 }
 
@@ -134,7 +134,7 @@ void Bmp::rotate(const char * targetFile){
 	for (int i=0; i<newHeight; ++i){
         for (int j=0; j<newWidth; ++j){
 			//cout << "i,j=" << i<< "," <<j<< endl;
-            *(target + i * newWidth + j) = *(data + j * width + newHeight - i - 1);
+            *(target + i * newWidth + j) = *(data + newHeight - i - 1 + j * width);
         }
     }
 	//cout << "旋转完成" << endl;
