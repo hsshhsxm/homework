d277f5415bd806dc77161aff2ecbad8b869fcdcf
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-05-03 18:47:27 +0800

    Update 9_A_List.cpp

--- program_pratice/9_A_List.cpp
+++ program_pratice/9_A_List.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <string>
 #include <list>
-#include <map>
 using namespace std;
 
 int main(){
-    map<int, list<int> > myMap;
+    list<int> myList[10000];
     int n;
-    string judge;
     cin >> n;
     while(n){
+        string judge;
         cin >> judge;
         if(judge == "new"){
             int id;
             cin >> id;
-            myMap.insert(pair<int, list<int> > (id, list<int>()));
         } else if(judge == "add"){
             int id,num;
             cin >> id >> num;
-            myMap[id].push_back(num);
-            myMap[id].sort();
+            myList[id].push_back(num);
+            myList[id].sort();
         } else if(judge == "merge"){
             int id1,id2;
             cin >> id1 >> id2;
-            myMap[id1].merge(myMap[id2]);
-            myMap[id2].clear();
+            myList[id1].merge(myList[id2]);
         } else if(judge == "unique"){
             int id;
             cin >> id;
-            myMap[id].unique();
+            //unique之前要sort
+            myList[id].sort();
+            myList[id].unique();
         } else if(judge == "out"){
             int id;
             cin >> id;
-            myMap[id].sort();
-            if(myMap.size() > 0){
-                list<int>::iterator it;
-                for(it = myMap[id].begin(); it != myMap[id].end(); ++it)
-                    cout << *it << " ";
-                cout << endl;
-            }
+            myList[id].sort();
+            list<int>::iterator it;
+            for(it = myList[id].begin(); it != myList[id].end(); ++it)
+                cout << *it << " ";
+            cout << endl;
         } else{}
         --n;
     }
