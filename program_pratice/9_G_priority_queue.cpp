461d511af6c6469d1e49d0ecc39144ea07a50d7b
Sullivan Suen <hsshhsxm@users.noreply.github.com>
2021-05-04 18:46:35 +0800

    finish 9G

--- program_pratice/9_G_priority_queue.cpp
+++ program_pratice/9_G_priority_queue.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <set>
+#include <cmath>
 using namespace std;
 
 bool judgePrime(int a){
-    for(int i = 2; i < a; ++i){
+    for(int i = 2; i <= sqrt((float)(a)); ++i){
         if(a % i == 0)
             return false;
     }
@@ -13,20 +14,28 @@ bool judgePrime(int a){
 struct findMax{
     bool operator()(int a, int b){
         int sumA = 0, sumB = 0;
-        for(int i = 2; i < a; ++i){
-            if(a % i == 0)
+        for(int i = 2; i <= sqrt((float)a); ++i){
+            if(a % i == 0){
+                int k = a / i;
+                if(i != k && judgePrime(k))
+                    ++sumA;
                 if(judgePrime(i))
                     ++sumA;
+            }
         }
-        for(int i = 2; i < b; ++i){
-            if(b % i == 0)
+        for(int i = 2; i < sqrt((float)b); ++i){
+            if(b % i == 0){
+                int k = b / i;
+                if(i != k && judgePrime(k))
+                    ++sumB;
                 if(judgePrime(i))
                     ++sumB;
+            }
         }
         if(sumA != sumB)
-            return sumA < sumB;
-        else 
-            return a < b;
+            return sumA > sumB;
+        else  
+            return a > b;
     }
 };
 
@@ -41,11 +50,11 @@ int main(){
             cin >> a;
             mySet.insert(a);
         }
-        int max = *(mySet.begin());
-        int min = *(mySet.rbegin());
-        cout << max << " " << min << endl;
-        mySet.erase(max);
-        mySet.erase(min);
+        int maxNum = *(mySet.begin());
+        int minNum = *(mySet.rbegin());
+        mySet.erase(maxNum);
+        mySet.erase(minNum);
+        cout << maxNum << " " << minNum << endl;
     }
     return 0;
 }
