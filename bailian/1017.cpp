7e35fd7f16bea4a5e7d8679d7e2bf8d9f7f2f30b
Sullivan Suen <hsshhsxm@gmail.com>
2021-05-13 11:30:49 +0000

    finish 1017.cpp

--- bailian/1017.cpp
+++ bailian/1017.cpp
@@ -13,14 +13,16 @@ int main()
         if(sum == 0)
             break;
         int boxNum = 0;
-        boxNum = a[5] + a[4] + a[3];
+        int u[4] = {0, 5, 3, 1};//装3*3后2x的2数目
+        boxNum = a[5] + a[4] + a[3] + (a[2] + 3) / 4;
         int l22 = 0, l11 = 0;
-        l22 = a[3] * 5;
-        l11 = a[4] * 11;
-        if(a[2] > 4){
-            boxNum += a[2] / 4;
-            a[2] -= a[2] / 4 * 4;
-        }
+        l22 = a[3] * 5 + u[a[2] % 4];
+        if(a[1] > l22)
+            boxNum += (a[1] - l22 + 8) / 9;
+        l11 = boxNum * 36 - a[5]*36 - a[4]*25 - a[3]*16 - a[2]*9 - a[1]*4;
+        if(a[0] > l11)
+            boxNum += (a[0] - l11 + 35) / 36;
+        cout << boxNum << endl;
     }
     return 0;
 }
