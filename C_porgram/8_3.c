d3ecb5746d3fa7b60b8846cab220ca6ca6d3dd83
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-05-21 12:38:13 +0800

    Update 8_3.c

--- C_porgram/8_3.c
+++ C_porgram/8_3.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 
-double solution(char str[], double low, double high){
+double solution(double (*p)(double), double low, double high){
     double n = 10000000;
     double h = (high - low) / n;
     double sum = 0;
-    double (*p)(double);
-    if(strcmp(str, "sin") == 0)
-        p = sin;
-    if(strcmp(str, "cos") == 0)
-        p = cos;
-    if(strcmp(str, "exp") == 0)
-        p = exp;
     for(double i = low; i < high; i+= h)
         sum += p(i) * h;
     return sum;
@@ -20,8 +12,8 @@ double solution(char str[], double low, double high){
 
 int main(){
     int a = 0, b = 1;
-    double ssin = solution("sin", a, b);
-    double scos = solution("cos", a, b);
-    double sexp = solution("exp", a, b);
+    double ssin = solution(sin, a, b);
+    double scos = solution(cos, a, b);
+    double sexp = solution(exp, a, b);
     printf("sin = %f\ncos = %f\nexp = %f\n",ssin, scos, sexp);
 }
