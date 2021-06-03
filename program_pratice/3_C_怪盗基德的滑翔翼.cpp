738eb08743bf7d66dcb63c9fca87ac66f2e67479
hsshhsxm <hsshhsxm@gmail.com>
2021-06-03 13:00:35 +0000

    finish 3C

--- program_pratice/3_C_怪盗基德的滑翔翼.cpp
+++ program_pratice/3_C_怪盗基德的滑翔翼.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
+    int K;
+    int N;
+    int h[100];
+    int road[100];
+    cin >> K;
+    while(K--){
+        memset(h, 0, sizeof(h));
+        memset(road, 0, sizeof(road));
+        cin >> N;
+        for(int i = 0; i < N; ++i)
+            cin >> h[i];
+        int res = 0;
+        for(int i = 0; i < N; ++i){
+            road[i] = 1;
+            for(int j = 0; j < i; ++j)
+                if(h[i] < h[j])
+                    road[i] = max(road[i], road[j] + 1);
+            res = max(res, road[i]);
+        }
+        for(int i = N - 1; i >= 0; --i){
+            road[i] = 1;
+            for(int j = N - 1; j > i; --j)
+                if(h[i] < h[j])
+                    road[i] = max(road[i], road[j] + 1);
+            res = max(res,road[i]);
+        }
+        cout << res << endl;
+    }
     return 0;
 }
 
