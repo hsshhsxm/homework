c749e359823673e4719371c21d03f2c8dac59239
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2021-06-04 16:00:15 +0800

    Create 1011_Sticks.cpp

---
+++ openjudge/poj/1011_Sticks.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define N 100
+int used[N], num[N];
+int n,sum;
+
+bool cmp(int a, int b){
+    return a < b;
+}
+
+int dfs(int l, int sum, int t){
+    
+}
+
+int main()
+{
+    while((cin >> n) != 0){
+
+    }
+    return 0;
+}
+
