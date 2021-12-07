e460a64183f1e570df0ff21af8d8222cb5916f52
Sullivan Suen <hsshhsxm@gmail.com>
2021-12-07 14:33:02 +0800

    add graphics project4

---
+++ Introduction_to_Computer_Graphics/graphics_project4/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace graphics_project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.双击以停止选择ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.BackColor = System.Drawing.Color.White;
+            this.PictureBox1.Location = new System.Drawing.Point(-14, -60);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(1920, 1080);
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDoubleClick);
+            this.PictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.PictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.PictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1894, 41);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(66, 35);
+            this.toolStripButton1.Text = "清除";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.双击以停止选择ToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(89, 44);
+            this.toolStripSplitButton1.Text = "帮助";
+            // 
+            // 双击以停止选择ToolStripMenuItem
+            // 
+            this.双击以停止选择ToolStripMenuItem.Name = "双击以停止选择ToolStripMenuItem";
+            this.双击以停止选择ToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.双击以停止选择ToolStripMenuItem.Text = "双击以停止选择";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.PictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem 双击以停止选择ToolStripMenuItem;
+    }
+}
+
---
+++ Introduction_to_Computer_Graphics/graphics_project4/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace graphics_project4
+{
+    public partial class Form1 : Form
+    {
+        List<PointF> points = new List<PointF>();
+        List<PointF> curve = new List<PointF>();
+        bool stopDraw = false;
+        bool changeCurve = false;
+        int selectedIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // click once to add a point or select a point
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!stopDraw)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    points.Add(e.Location);
+                    points = points.Distinct().ToList();
+                }
+                if (points.Count > 2)
+                {
+                    DrawCurve(PictureBox1.CreateGraphics());
+                }
+            }
+            if (stopDraw)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (Math.Abs(e.Location.X - points[i].X) < 10 && Math.Abs(e.Location.Y - points[i].Y) < 10)
+                    {
+                        selectedIndex = i;
+                        changeCurve = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        // end change curve
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (stopDraw && changeCurve)
+            {
+                points[selectedIndex] = e.Location;
+                DrawFinalLine(PictureBox1.CreateGraphics());
+                changeCurve = false;
+            }
+        }
+
+        // click twice to end
+        private void PictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (points.Count < 3)
+            {
+                MessageBox.Show("At least 3 points!");
+                return;
+            }
+            if (!stopDraw)
+            {
+                points.Add(e.Location);
+                points = points.Distinct().ToList();
+                DrawFinalLine(PictureBox1.CreateGraphics());
+            }
+            stopDraw = true;
+        }
+
+        // move mouse to select point or change curve
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (points.Count != 0 && !stopDraw)
+            {
+                DrawRubberLine(PictureBox1.CreateGraphics(), points.Last(), e.Location);
+            }
+            if (stopDraw && changeCurve)
+            {
+                points[selectedIndex] = e.Location;
+                DrawFinalLine(PictureBox1.CreateGraphics());
+                DrawCurve(PictureBox1.CreateGraphics());
+            }
+        }
+
+        // draw lines, rubber band
+        private void DrawRubberLine(Graphics g, PointF start, PointF end)
+        {
+            BufferedGraphicsContext context = BufferedGraphicsManager.Current;
+            BufferedGraphics bg = context.Allocate(g, new Rectangle(0, 0, PictureBox1.Width, PictureBox1.Height));
+            bg.Graphics.Clear(Color.White);
+            for (int i = 0; i < points.Count - 1; ++i)
+            {
+                bg.Graphics.DrawLine(SystemPens.ControlText, points[i], points[i+1]);
+                bg.Graphics.FillRectangle(Brushes.Black, points[i].X, points[i].Y, 4, 4);
+            }
+            Pen redPen = new Pen(Color.Red);
+            for (int i = 0; i < curve.Count; i += 2)
+            {
+                bg.Graphics.DrawLine(redPen, curve[i], curve[i + 1]);
+            }
+            bg.Graphics.DrawLine(SystemPens.ControlText, start, end);
+            bg.Graphics.FillRectangle(Brushes.Black, points.Last().X, points.Last().Y, 4, 4);
+            bg.Render();
+            bg.Dispose();
+        }
+
+        private void DrawFinalLine(Graphics g)
+        {
+            BufferedGraphicsContext context = BufferedGraphicsManager.Current;
+            BufferedGraphics bg = context.Allocate(g, new Rectangle(0, 0, PictureBox1.Width, PictureBox1.Height));
+            bg.Graphics.Clear(Color.White);
+            for (int i = 0; i < points.Count - 1; ++i)
+            {
+                bg.Graphics.DrawLine(SystemPens.ControlText, points[i], points[i + 1]);
+                bg.Graphics.FillRectangle(Brushes.Black, points[i].X, points[i].Y, 4, 4);
+                if (i == points.Count - 2)
+                {
+                    bg.Graphics.FillRectangle(Brushes.Black, points[i+1].X, points[i+1].Y, 4, 4);
+                }
+            }
+            Pen redPen = new Pen(Color.Red);
+            for (int i = 0; i < curve.Count; i += 2)
+            {
+                bg.Graphics.DrawLine(redPen, curve[i], curve[i + 1]);
+            }
+            bg.Render();
+            bg.Dispose();
+        }
+
+        private void CurveSplit(List<PointF> P, ref List<PointF> Q, ref List<PointF> R)
+        {
+            int n = P.Count - 1;
+            for (int i = 0; i <= n; i++)
+            {
+                Q.Add(P[i]);
+            }
+            for (int i = 0; i <= n; ++i)
+            {
+                R.Add(new PointF());
+            }
+            for (int i = 1; i <= n; ++i)
+            {
+                R[n + 1 - i] = Q[n];
+                for (int j = n; j >= i; --j)
+                {
+                    PointF tmp = new PointF((Q[j - 1].X + Q[j].X) / 2, (Q[j - 1].Y + Q[j].Y) / 2);
+                    Q[j] = tmp;
+                }
+            }
+            R[0] = Q[n];
+        }
+
+        private void DrawCurve(Graphics g)
+        {
+            if (points.Count < 3)
+                return;
+            curve.Clear();
+            List<PointF> P = new List<PointF>();
+            for (int i = 0; i < points.Count; ++i)
+            {
+                P.Add(points[i]);
+            }
+            List<PointF> Q = new List<PointF>();
+            List<PointF> R = new List<PointF>();
+            List<PointF> P1 = new List<PointF>();
+            Stack<List<PointF>> stack = new Stack<List<PointF>>();
+            stack.Push(P);
+            while (stack.Count > 0)
+            {
+                P1 = stack.Pop();
+                float maxD = 0;
+                int n = P1.Count - 1;
+                for (int i = 1; i <= n - 1; ++i)
+                {
+                    if (CountDistance(P1[i], P1[0], P1[n]) > maxD)
+                    {
+                        maxD = CountDistance(P1[i], P1[0], P1[n]);
+                    }
+                }
+                if (maxD <= 1)
+                {
+                    curve.Add(P1[0]);
+                    curve.Add(P1[n]);
+                }
+                else
+                {
+                    CurveSplit(P1, ref Q, ref R);
+                    List <PointF> tmpQ = new List<PointF>();
+                    for (int i = 0; i < Q.Count; ++i)
+                    {
+                        tmpQ.Add(Q[i]);
+                    }
+                    List<PointF> tmpR = new List<PointF>();
+                    for (int i = 0; i < R.Count; ++i)
+                    {
+                        tmpR.Add(R[i]);
+                    }
+                    stack.Push(tmpQ);
+                    stack.Push(tmpR);
+                    Q.Clear();
+                    R.Clear();
+                }
+            }
+        }
+
+        private float CountDistance(PointF pi, PointF p0, PointF pn)
+        {
+            double a = Math.Sqrt(Math.Pow(pi.X - p0.X, 2) + Math.Pow(pi.Y - p0.Y, 2));
+            double b = Math.Sqrt(Math.Pow(pi.X - pn.X, 2) + Math.Pow(pi.Y - pn.Y, 2));
+            double c = Math.Sqrt(Math.Pow(pn.X - p0.X, 2) + Math.Pow(pn.Y - p0.Y, 2));
+            double s = (a + b + c) / 2;
+            double d = 2 * Math.Sqrt(s * (s - a) * (s - b) * (s - c)) / c;
+            return (float)d;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            curve.Clear();
+            DrawFinalLine(PictureBox1.CreateGraphics());
+            stopDraw = false;
+            changeCurve = false;
+            selectedIndex = -1;
+        }
+    }
+}
